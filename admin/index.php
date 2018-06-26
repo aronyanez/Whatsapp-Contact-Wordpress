31ad146b0cbde097642d0bf5985bac221f7fc36b
aronyanez <30335407+aronyanez@users.noreply.github.com>
2018-06-26 17:48:46 -0500

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,45 @@
+<?php
+add_action( 'admin_menu', 'Whats_create_admin_menu');
+add_action('admin_init','Whats_settings');
+function Whats_create_admin_menu() {
+	add_menu_page (
+		'Whats Plugin', 'Whats Plugin', 'administrator', 'Whats_opciones', 'Whats_create_admin_menu_function', '
+		dashicons-align-center' );
+}
+
+
+function Whats_create_admin_menu_function() {
+	?>
+	<div class='wrap'>
+		<h2>  <?= _e('Whatsapp settings','mark_whatsapp'); ?></h2>
+		<form action="options.php" method="POST">
+			<?php
+			settings_fields('Whats_group');
+			do_settings_sections('Whats_group');
+			?>
+      <label><?= _e('Phone Number','mark_whatsapp');  ?></label>
+      <input type="number"
+      name="Whats_number"
+      id="Whats_number"
+      value="<?= get_option('Whats_number'); ?>">
+     
+
+
+    <?= submit_button(); ?>
+  </form>
+
+   <h3 class="credits"><?= _e('Plugin by','mark_whatsapp');  ?>
+     <a href="https://markdevs.com">Arón Yáñez</a>
+   </h3>
+</div>
+
+<?php
+}
+function Whats_settings(){
+  $args = array(
+    'type' => 'number', 
+    'sanitize_callback' => 'sanitize_text_field',
+    'default' => NULL,
+  );
+  register_setting('Whats_group','Whats_number',$args);
+}
---
+++ public/index.php
@@ -0,0 +1,66 @@
+<?php
+
+
+
+
+
+
+
+add_action( 'get_footer', 'Whats_code', 1);
+
+
+
+
+
+
+
+add_action( 'wp_enqueue_scripts', 'Whats_styles' );
+
+
+
+
+
+
+
+
+
+function Whats_styles() {
+
+	wp_enqueue_style( 'style-custom', plugins_url( 'mark_whatsapp/public/css/style-f.css' ) );
+
+}
+
+
+
+function Whats_code() {
+
+	//if (is_home()){
+
+		?>
+
+
+
+   
+
+
+
+      <a class="whats whatapp-movil" href="whatsapp://send/?phone=52<?php echo get_option('Whats_number'); ?>&amp;text=Hola Necesito informes" target="_blank">
+
+	 <img src="<?php echo plugins_url( 'mark_whatsapp/public/img/icon.png' ) ?>">
+
+      Whatsapp</a>
+
+         <a class="whats whatsapp-web" href="https://web.whatsapp.com/send?phone=+52<?php echo get_option('Whats_number'); ?>&amp;text=Hola Necesito informes" target="_blank">
+
+	 <img src="<?php echo plugins_url( 'mark_whatsapp/public/img/icon.png' ) ?>">
+
+      Whatsapp</a>
+
+
+      <?php
+
+	//}
+
+}
+
+
